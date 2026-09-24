188ddd8795a950feca84d39e51c38a38829b2d79
Prakhar singh <102253372+prakharsingh123@users.noreply.github.com>
2026-09-25 02:26:21 +0530

    Time: 2 ms (86.28%) | Memory: 49.6 MB (14.35%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -10,36 +10,49 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        Stack<ListNode> stack = new Stack<>();
 
-        ListNode curr = head;
-        while(curr!=null){
-           stack.push(curr);
-           curr = curr.next;
+         if(head == null || head.next == null) {
+            return;
+        }
+    
+     ListNode slow = head;
+     ListNode fast = head;
+
+     while(fast.next!=null && fast.next.next!=null){
+         slow = slow.next;
+         fast = fast.next.next;
+     }
+     ListNode newHead = reverse(slow.next);
+     slow.next = null;
+
+     ListNode curr = head;
+     ListNode rev = newHead;
+
+
+     while(rev!=null){
+        ListNode tempRev = rev.next;
+        ListNode temp = curr.next;
+         curr.next = rev;
+         rev.next = temp;
+         curr = temp;
+         rev = tempRev;
+
+     }
+
+    }
+    public ListNode reverse(ListNode head){
+        
+    
+
+        if(head==null || head.next==null){
+            return head;
         }
 
-        curr = head;
+            ListNode n = reverse(head.next);
 
-         int size = stack.size();
+        head.next.next = head;
+        head.next = null;
 
-         for(int i=0;i<size/2;i++){
-
-            ListNode last = stack.pop();
-            ListNode next = curr.next;
-
-            // curr anhi head pr hai 
-            // curr.next last wale ko pakdega 
-
-            curr.next = last;
-            last.next = next;
-
-            curr = next;
-           
-
-
-         }
-
-         curr.next = null;
-
+        return n;
     }
 }
